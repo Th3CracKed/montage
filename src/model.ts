4fa8670c00731d86231073462eae7c7b30d13686
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-06-13 23:14:49 +0200

    first working version that support the highest common quality between all videos

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,12 @@
+import { videoFormat } from "ytdl-core";
+
 export interface Video {
     url: string;
+    start: string;
+    duration: string;
+}
+export interface CustomVideoFormat extends videoFormat {
+    ytUrl: string;
     start: string;
     duration: string;
 }
