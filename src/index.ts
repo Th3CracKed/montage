a695ff3d5e720e9cacb742b615ccd6584d213d52
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-06-15 00:51:04 +0200

    same results with highest selected quality from ytdl because the really highest quality is a sepetrate stream of audio and video that should be combined

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 import fsExtra from 'fs-extra';
 
-import ytdl, { videoFormat } from 'ytdl-core';
+import ytdl from 'ytdl-core';
 
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 import ffmpeg from 'fluent-ffmpeg'
 import data from './input';
-import { CustomVideoFormat, Video } from './model';
+import { Video } from './model';
 
 (async () => {
     ffmpeg.setFfmpegPath(ffmpegPath)
     fsExtra.emptyDirSync('videos');
-    const videosFormats = await getHighestVideosFormats(data);
     try {
-        const videosPaths = await downloadDefinedVideoChunks(videosFormats);
+        const videosPaths = await downloadDefinedVideoChunks(data);
         mergeVideos(videosPaths, 'videos/out.mp4');
     } catch (err) {
         console.log(err);
     }
 })()
 
-async function downloadDefinedVideoChunks(videosFormats: CustomVideoFormat[]) {
+async function downloadDefinedVideoChunks(videosFormats: Video[]) {
     console.log('Downloading Videos...');
-    const videoChunksPromises = videosFormats.map(async (videoFormat, index) => {
+    const videoChunksPromises = videosFormats.map(async (video, index) => {
         const videoPath = 'videos/' + index + '.mp4';
-        await downloadYtVideo(videoPath, videoFormat);
+        await downloadYtVideo(videoPath, video.url);
         const tmpPath = videoPath.replace('.mp4', '_tmp.mp4');
-        const isCut = await cutVideo(videoPath, tmpPath, videoFormat.start, videoFormat.duration);
+        const isCut = await cutVideo(videoPath, tmpPath, video.start, video.duration);
         if (!isCut) {
             throw Error('Error while cutting video');
         }
@@ -37,10 +36,10 @@ async function downloadDefinedVideoChunks(videosFormats: CustomVideoFormat[]) {
     return Promise.all(videoChunksPromises);
 }
 
-function downloadYtVideo(videoPath: string, videoFormat: CustomVideoFormat): Promise<void> {
-    console.log(`Downloading : ${videoFormat.ytUrl} as ${videoPath}`);
+function downloadYtVideo(videoPath: string, url: string): Promise<void> {
+    console.log(`Downloading : ${url} as ${videoPath}`);
     return new Promise<void>(async (resolve, reject) => {
-        ytdl(videoFormat.ytUrl, { quality: 'highestvideo', filter: 'audioandvideo', format: videoFormat })
+        ytdl(url, { quality: 'highestvideo', filter: 'audioandvideo' })
             .pipe(fsExtra.createWriteStream(videoPath))
             .on('close', () => {
                 console.log('Downloading finished for :', videoPath);
@@ -48,37 +47,6 @@ function downloadYtVideo(videoPath: string, videoFormat: CustomVideoFormat): Pro
             })
             .on('error', reject);
     });
-}
-
-async function getHighestVideosFormats(videos: Video[]): Promise<CustomVideoFormat[]> {
-    return Promise.all(videos.map(async video => {
-        const videoInfo = await ytdl.getInfo(video.url);
-        const highestVideoFormat = getHighestValidVideoFormat(videoInfo.formats);
-        return { ...highestVideoFormat, ytUrl: video.url, start: video.start, duration: video.duration };
-    }));
-}
-
-function getHighestValidVideoFormat(videosFormats: ytdl.videoFormat[]) {
-    const map = videosFormats.reduce((accMap: { [key: string]: ytdl.videoFormat }, videoFormat) => {
-        const { qualityLabel, hasAudio, hasVideo, container } = videoFormat;
-        const isValidVideoFormat = qualityLabel && hasAudio && hasVideo && container === 'mp4';
-        const isExistingFormat = accMap[qualityLabel];
-        if (isValidVideoFormat && !isExistingFormat) {
-            accMap[qualityLabel] = videoFormat;
-        }
-        return accMap;
-    }, {});
-    return chooseVideoFormatToDownload(map);
-}
-
-function chooseVideoFormatToDownload(map: { [key: string]: ytdl.videoFormat }): ytdl.videoFormat {
-    const resolutions = ['2160p60', '2160p', '1440p60', '1440p', '1080p60', '1080p', '720p60', '720p', '480p', '360p', '240p', '144p'];
-    while (resolutions.length > 0) {
-        const qualityLabel = resolutions.shift();
-        if (map[qualityLabel]) {
-            return map[qualityLabel];
-        }
-    }
 }
 
 function cutVideo(path: string, outputPath: string, startTime: string, duration: string): Promise<boolean> {
