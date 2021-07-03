572bf76a39f424af38c12d2389e42cc5174105a7
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-07-03 02:05:37 +0200

    retry strategy

--- src/utils.ts
+++ src/utils.ts
@@ -3,18 +3,8 @@
  * @param seconds 
  * @returns 
  */
-export function formatDuration(seconds: number | string): string {
-    var parts = []
-    parts.push(Number(seconds) % 60)
-    var minutes = Math.floor(Number(seconds) / 60)
-    if (minutes > 0) {
-        parts.push(minutes % 60)
-        var hours = Math.floor(minutes / 60)
-        if (hours > 0) {
-            parts.push(hours)
-        }
-    }
-    return parts.reverse().join(':')
+export function formatDuration(seconds: number): string {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
 }
 
 
