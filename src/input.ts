bfd2c53d7ae697e5d413211f08938533220eaab7
DRIOWYA Abdelghafour <abdodriowya@gmail.com>
2021-07-04 11:49:11 +0200

    ratchet serie

--- src/input.ts
+++ src/input.ts
@@ -4,80 +4,80 @@ const videos: Video[] = [
     {
         "url": "https://www.youtube.com/watch?v=FNG6iafLxVE",
         "start": "00:06:27",
-        "duration": "20",
+        "duration": "30",
         "text": "Ratchet & Clank 2002"
     },
     {
         "url": "https://www.youtube.com/watch?v=lX1YHAdK-nw",
-        "start": "00:03:30",
-        "duration": "20",
-        "text": "Ratchet & Clank: Going Commando 2003"
+        "start": "00:05:00",
+        "duration": "30",
+        "text": "Ratchet & Clank Going Commando 2003"
     },
     {
         "url": "https://www.youtube.com/watch?v=cxL_pLkn5X4",
         "start": "00:04:15",
-        "duration": "20",
-        "text": "Ratchet & Clank: Up Your Arsenal 2004"
+        "duration": "30",
+        "text": "Ratchet & Clank Up Your Arsenal 2004"
     },
     {
         "url": "https://www.youtube.com/watch?v=KnlRqPh90qI",
         "start": "00:03:30",
-        "duration": "20",
+        "duration": "30",
         "text": "Ratchet: Deadlocked 2005"
     },
-    // {
-    //     "url": "https://www.youtube.com/watch?v=_LptCBbd87k",
-    //     "start": "00:02:00",
-    //     "duration": "20",
-    //     "text": "Ratchet and Clank: Size Matters 2007"
-    // },
+    {
+        "url": "https://www.youtube.com/watch?v=COVjh77JI7g",
+        "start": "00:01:28",
+        "duration": "20",
+        "text": "Ratchet and Clank Size Matters 2007"
+    },
     {
         "url": "https://www.youtube.com/watch?v=nu9lmT5dawY",
         "start": "00:07:30",
-        "duration": "20",
-        "text": "Ratchet & Clank Future: Tools of Destruction 2007"
+        "duration": "30",
+        "text": "Ratchet & Clank Future Tools of Destruction 2007"
     },
     {
         "url": "https://www.youtube.com/watch?v=MlWiDsgZX_4",
         "start": "00:07:49",
-        "duration": "20",
-        "text": "Ratchet & Clank Future: Quest for Booty 2008"
+        "duration": "30",
+        "text": "Ratchet & Clank Future Quest for Booty 2008"
     },
     {
         "url": "https://www.youtube.com/watch?v=AFnY9iBqgUk",
         "start": "00:05:01",
-        "duration": "20",
-        "text": "Ratchet & Clank: A Crack 2009"
+        "duration": "30",
+        "text": "Ratchet & Clank A Crack 2009"
     },
     {
         "url": "https://www.youtube.com/watch?v=tUu4s6eozAI",
         "start": "00:04:50",
-        "duration": "20",
-        "text": "Ratchet & Clank: All 4 One 2011"
+        "duration": "30",
+        "text": "Ratchet & Clank All 4 One 2011"
     },
     {
         "url": "https://www.youtube.com/watch?v=7jiN-LnccW0",
         "start": "00:08:30",
-        "duration": "20",
-        "text": "Ratchet and Clank: Full Frontal Assault 2012"
+        "duration": "30",
+        "text": "Ratchet and Clank Full Frontal Assault 2012"
     },
     {
         "url": "https://www.youtube.com/watch?v=akQTYUqpmJo",
         "start": "00:09:00",
-        "duration": "20",
-        "text": "Ratchet and Clank: Into the Nexus 2013"
+        "duration": "30",
+        "text": "Ratchet and Clank Into the Nexus 2013"
     },
     {
         "url": "https://www.youtube.com/watch?v=Z-3pm3id1Rk",
         "start": "00:12:19",
-        "duration": "20",
+        "duration": "30",
         "text": "Ratchet & Clank 2016"
     },
     {
         "url": "https://www.youtube.com/watch?v=WbENbj5I4rY",
         "start": "00:03:02",
-        "duration": "20",
-        "text": "Ratchet and Clank: Rift Apart 2021"
+        "duration": "30",
+        "text": "Ratchet and Clank Rift Apart 2021"
     },
 ];
 
